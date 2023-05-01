52eb781f53a615332149d00cb575068b88919434
Alom Wisdom <ogwisdom27@gmail.com>
2023-05-02 00:54:15 +0100

    Added font-icon

--- src/components/Navbar2.jsx
+++ src/components/Navbar2.jsx
@@ -1,6 +1,9 @@
 import React,{useState} from 'react';
 import { NavLink } from 'react-router-dom';
 import logo2 from '../assets/images/logo-1.png'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faXmark } from '@fortawesome/free-solid-svg-icons'
+import { faBarsStaggered } from '@fortawesome/free-solid-svg-icons'
 
 export const Navbar2 = () => {
   const [active, setActive] = useState(false)
@@ -8,6 +11,9 @@ export const Navbar2 = () => {
   const showMenu = () => {
      setActive(!active)
   }
+
+  // const element = <FontAwesomeIcon icon={faBars} />
+
 
   const navLinkStyles = ({isActive}) => {
     return {
@@ -21,7 +27,7 @@ export const Navbar2 = () => {
     <div className='header'>
         <div className="largeSc">
         <div className='logo'><img src={logo2}height={40} alt="logo"/></div>
-        <div className="showmenu" onClick={showMenu}>{active ? "+" : "=" }</div>
+        <div className="showmenu" onClick={showMenu}>{active ? <FontAwesomeIcon icon={faXmark} />: <FontAwesomeIcon icon={faBarsStaggered} />}</div>
         </div>
         <nav className={active ? "slider active" : "slider"}>
             <div className="logoLx"> <img src={logo2} height={40} alt="logo"/></div>
