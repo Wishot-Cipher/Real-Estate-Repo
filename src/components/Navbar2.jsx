d8639e26f52e75fc16929db536aa8450e22f0858
Alom Wisdom <ogwisdom27@gmail.com>
2023-05-03 00:08:46 +0100

    made nav to close when cliked out the box

--- src/components/Navbar2.jsx
+++ src/components/Navbar2.jsx
@@ -1,24 +1,38 @@
-import React,{useState} from 'react';
+import React,{useState, useEffect, useRef} from 'react';
 import { NavLink } from 'react-router-dom';
 import logo2 from '../assets/images/logo-1.png'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faXmark } from '@fortawesome/free-solid-svg-icons'
 import { faBarsStaggered } from '@fortawesome/free-solid-svg-icons'
+
 
 export const Navbar2 = () => {
   const [active, setActive] = useState(false)
+   const ref = useRef(null)
 
-  const showMenu = () => {
-     setActive(!active)
+   const showMenu = () => {
+    setActive(!active)
   }
 
-  // const element = <FontAwesomeIcon icon={faBars} />
+   const handleClickOutside = (event) => {
+    if ( ref.current && !ref.current.contains(event.target)) {
+        setActive(false)
+      // alert("cliked");
+    }
+  } ; 
 
+  useEffect(() => {
+    document.addEventListener("mousedown", handleClickOutside);
+
+    return () =>{
+      document.removeEventListener("mousedown", handleClickOutside)
+    }
+  
+  }, [active])
 
   const navLinkStyles = ({isActive}) => {
     return {
       fontWeight : isActive ? "bold" : "normal",
-      color : isActive ? '#FF5A5F' : 'black',
+      color : isActive ? '#FF5363' : 'rgba(5, 5, 5, 0.7)',
       textDecoration: isActive ? 'none':'none',
     }
   }
@@ -27,26 +41,27 @@ export const Navbar2 = () => {
     <div className='header'>
         <div className="largeSc">
         <div className='logo'><img src={logo2}height={40} alt="logo"/></div>
-        <div className="showmenu" onClick={showMenu}>{active ? <FontAwesomeIcon icon={faXmark} />: <FontAwesomeIcon icon={faBarsStaggered} />}</div>
+        <div className="showmenu" onClick={showMenu}> <FontAwesomeIcon icon={faBarsStaggered} /></div>
         </div>
-        <nav className={active ? "slider active" : "slider"}>
+        <nav className={active ? "slider active" : "slider"} ref={ref}>
             <div className="logoLx"> <img src={logo2} height={40} alt="logo"/></div>
             <ul>
-                <li>
-                    <NavLink to="/" onClick={showMenu} style={navLinkStyles}>Home </NavLink>
-                </li>
-                <li>
-                    <NavLink to="/members" onClick={showMenu} style={navLinkStyles}>Members </NavLink>
-                </li>
-                <li>
-                    <NavLink to="/about" onClick={showMenu} style={navLinkStyles}> About </NavLink>
-                </li>
-                <li>
-                    <NavLink to="/contact" onClick={showMenu} style={navLinkStyles}> Contact </NavLink>
-                </li>
-                <li>
-                    <NavLink to="/register" onClick={showMenu} style={navLinkStyles}>Register </NavLink>
-                </li>
+                <li className='menu'> Menu</li>
+                <NavLink to="/" onClick={showMenu} style={navLinkStyles}> 
+                    <li> Home </li>
+                </NavLink>
+                <NavLink to="/members" onClick={showMenu} style={navLinkStyles}>
+                    <li>  Members  </li>
+                </NavLink>
+                <NavLink to="/about" onClick={showMenu} style={navLinkStyles}> 
+                    <li>  About  </li> 
+                </NavLink>
+                <NavLink to="/contact" onClick={showMenu} style={navLinkStyles}> 
+                    <li>  Contact  </li>
+                 </NavLink>
+                 <NavLink to="/register" onClick={showMenu} style={navLinkStyles}>
+                    <li className="lastLink">  Register  </li> 
+                 </NavLink>
             </ul>
         </nav>
     </div>
